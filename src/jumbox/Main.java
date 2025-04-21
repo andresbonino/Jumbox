a52054205063a31a4ea4b7de848bba73781ece63
MatiToro <matias.asantoro@davinci.edu.ar>
2025-04-21 12:47:20 -0300

    v1

---
+++ src/jumbox/Main.java
@@ -0,0 +1,13 @@
+package jumbox;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Hola");
+	}
+}
