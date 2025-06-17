dacfb8c3039020dc17ff9bcba9457fe0abe77a0a
marcoskleinn <marcos.klein@davinci.edu.ar>
2025-06-17 11:22:55 -0300

    as

--- JumboxV18/src/jumbox/Productos.java
+++ JumboxV18/src/jumbox/Productos.java
@@ -1,5 +1,7 @@
 package jumbox;
 
+
+
 import javax.swing.JOptionPane;
 
 import DDL.ControllerProducto;
---
+++ JumboxV18/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module jumbox {
+	requires java.desktop;
+	requires mysql.connector.java;
+	requires java.sql;
+}
