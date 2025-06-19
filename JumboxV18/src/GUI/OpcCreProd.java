e70d650cbad9928196de7715bb1635cc7b987d7c
depo <andres.bonino@davinci.edu.ar>
2025-06-19 11:58:02 -0300

    OpcreProd

--- JumboxV18/src/GUI/OpcCreProd.java
+++ JumboxV18/src/GUI/OpcCreProd.java
@@ -9,6 +9,8 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JComboBox;
 
 public class OpcCreProd extends JFrame {
 
@@ -39,7 +41,7 @@ public class OpcCreProd extends JFrame {
 	 */
 	public OpcCreProd() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 638, 848);
+		setBounds(100, 100, 680, 922);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -47,38 +49,85 @@ public class OpcCreProd extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel_2 = new JLabel("Ingrese nombre");
-		lblNewLabel_2.setBounds(190, 217, 248, 45);
+		lblNewLabel_2.setBounds(190, 188, 248, 45);
 		lblNewLabel_2.setFont(new Font("Leelawadee UI", Font.BOLD, 25));
 		contentPane.add(lblNewLabel_2);
 		
 		passwordField = new JPasswordField();
+		passwordField.setBounds(80, 244, 456, 39);
 		passwordField.setFont(new Font("Arial", Font.PLAIN, 18));
-		passwordField.setBounds(80, 286, 456, 39);
 		contentPane.add(passwordField);
 		
 		JLabel lblNewLabel_2_1 = new JLabel("Ingrese precio");
+		lblNewLabel_2_1.setBounds(190, 334, 248, 45);
 		lblNewLabel_2_1.setFont(new Font("Leelawadee UI", Font.BOLD, 25));
-		lblNewLabel_2_1.setBounds(190, 383, 248, 45);
 		contentPane.add(lblNewLabel_2_1);
 		
 		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(80, 390, 456, 39);
 		passwordField_1.setFont(new Font("Arial", Font.PLAIN, 18));
-		passwordField_1.setBounds(80, 452, 456, 39);
 		contentPane.add(passwordField_1);
 		
 		JLabel lblNewLabel_2_1_1 = new JLabel("Ingrese stock");
+		lblNewLabel_2_1_1.setBounds(203, 482, 248, 45);
 		lblNewLabel_2_1_1.setFont(new Font("Leelawadee UI", Font.BOLD, 25));
-		lblNewLabel_2_1_1.setBounds(190, 540, 248, 45);
 		contentPane.add(lblNewLabel_2_1_1);
 		
 		passwordField_2 = new JPasswordField();
+		passwordField_2.setBounds(80, 538, 456, 39);
 		passwordField_2.setFont(new Font("Arial", Font.PLAIN, 18));
-		passwordField_2.setBounds(80, 604, 456, 39);
 		contentPane.add(passwordField_2);
 		
 		JButton btnAceptar = new JButton("Aceptar");
-		btnAceptar.setFont(new Font("Dialog", Font.PLAIN, 22));
-		btnAceptar.setBounds(211, 712, 158, 64);
+		btnAceptar.setBounds(241, 791, 141, 64);
+		btnAceptar.setFont(new Font("Dialog", Font.PLAIN, 20));
 		contentPane.add(btnAceptar);
+		
+		JLabel lblNewLabel_1 = new JLabel("Deposito");
+		lblNewLabel_1.setBounds(76, 20, 431, 157);
+		lblNewLabel_1.setForeground(new Color(0, 128, 0));
+		lblNewLabel_1.setFont(new Font("Dialog", Font.BOLD, 90));
+		contentPane.add(lblNewLabel_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(241, 695, 155, 55);
+		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		comboBox.addItem("Mueble");
+		comboBox.addItem("Electrodomestico");
+		comboBox.addItem("Alimento");
+		comboBox.addItem("Limpieza");
+		comboBox.addItem("Higiene_Personal");
+		comboBox.addItem("Farmacia");
+		comboBox.addItem("Mascotas");
+		comboBox.addItem("Hogar");
+		comboBox.addItem("Ferreteria");
+		comboBox.addItem("Jugueteria");
+		comboBox.addItem("Libreria");
+		comboBox.addItem("Bebidas");
+		comboBox.addItem("Despensa");
+		contentPane.add(comboBox);
+		
+		JLabel lblNewLabel_2_1_2 = new JLabel("¿En que categoria entra tu producto?");
+		lblNewLabel_2_1_2.setFont(new Font("Leelawadee UI", Font.BOLD, 25));
+		lblNewLabel_2_1_2.setBounds(92, 639, 444, 45);
+		contentPane.add(lblNewLabel_2_1_2);
+		
+		JLabel LblError = new JLabel("");
+		LblError.setForeground(Color.RED);
+		LblError.setFont(new Font("Arial", Font.PLAIN, 15));
+		LblError.setBounds(130, 294, 354, 29);
+		contentPane.add(LblError);
+		
+		JLabel LblError_1 = new JLabel("");
+		LblError_1.setForeground(Color.RED);
+		LblError_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		LblError_1.setBounds(115, 440, 354, 31);
+		contentPane.add(LblError_1);
+		
+		JLabel LblError_2 = new JLabel("");
+		LblError_2.setForeground(Color.RED);
+		LblError_2.setFont(new Font("Arial", Font.PLAIN, 15));
+		LblError_2.setBounds(115, 588, 354, 40);
+		contentPane.add(LblError_2);
 	}
 }
